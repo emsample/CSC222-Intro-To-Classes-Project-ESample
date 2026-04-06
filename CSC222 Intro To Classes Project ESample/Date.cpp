dcd0c7bf409974dfdd69e4b34dff943d375fe7fc
emsample <esamplebox@gmail.com>
2026-04-06 19:50:04 -0400

    So i satrted adding the logic for my  validation functions starting with the lastDay function. Now I need to make the logic for the leap year check.

--- CSC222 Intro To Classes Project ESample/Date.cpp
+++ CSC222 Intro To Classes Project ESample/Date.cpp
@@ -3,14 +3,17 @@
 
 Date::Date(int m = 1, int d = 1, int y = 1900)
 {
-	
+	setDate(m, d, y);
 }
 void Date::setDate(int m, int d, int y)
 {
-	if (m < 1 || m > 12 || d < 1 || d < lastDay(month, year) || y < 1)
+	if (month < 1 || month > 12 || day < 1 || day < lastDay(month, year) || year < 1)
 	{
-
+		month = m;
+		day = d;
+		year = y; 
 	}
+
 }
 int Date::getMonth() const
 {
@@ -25,3 +28,32 @@ int Date::getYear() const
 	return year;
 }
 int lastDay(int month, int year)
+{
+	if (month == 2)
+	{
+		if (!isLeapyear())
+		{
+			return 28;
+		}
+		else
+		{
+			return 29;
+		}
+	}
+	else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+	{
+		return 31;
+	}
+	else if (month == 4 || month == 6 || month == 9 || month == 11 || )
+	{
+		return 30;
+	}
+}
+bool Date::isLeapYear() const
+{
+	isLeapYear(year);
+}
+bool Date::isLeapYear(int year) const
+{
+
+}
